50cdd7b5559b285bdd4f6fc13fbd437407f2293d
Dhruba Ghosh <djghosh13@gmail.com>
2019-09-22 13:11:10 -0700

    Update common information

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     setInterval($.get, 2000, "streaminfo.json", "", function(data) {
-        $("#title").text(data["title"]);
+        // Global information
+        $("#title").html(data["title"]);
+        $("#casters").html(data["casters"]);
+        $("#next-song").html(data["nextsong"]);
+        // Song information
+        for (let key in data["song"]) {
+            $("#song-" + key).html(data["song"][key]);
+        }
+        // Stream information
+        for (let side of ["left", "right"]) {
+            
+        }
     }, "json");
 });
