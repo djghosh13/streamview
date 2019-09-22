c811d0ac3eb8d23f8248eae17c535c09bcc223f8
Dhruba Ghosh <djghosh13@gmail.com>
2019-09-22 14:47:04 -0700

    Update point lights

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ var currentStream = {
     "right": ""
 };
 
+function update(field, value) {
+    if (field.html() != value) {
+        field.html(value);
+    }
+}
+
 $(document).ready(function() {
-    setInterval($.get, 2000, "streaminfo.json", "", function(data) {
+    setInterval($.get, 2000, "streaminfo.json", { dummy: Math.random() }, function(data) {
         // Global information
-        $("#title").html(data["title"]);
-        $("#casters").html(data["casters"]);
-        $("#next-song").html(data["nextsong"]);
+        update($("#title"), data["title"]);
+        update($("#casters"), data["casters"]);
+        update($("#next-song"), data["nextsong"]);
         // Song information
         for (let key in data["song"]) {
-            $("#song-" + key).html(data["song"][key]);
+            update($("#song-" + key), data["song"][key]);
         }
         // Stream information
         for (let side of ["left", "right"]) {
-            $("#streamer-" + side).html(data[side]["name"]);
+            update($("#streamer-" + side), data[side]["name"]);
             if (currentStream[side] != data[side]["stream"]) {
                 currentStream[side] = data[side]["stream"];
                 $("#player-" + side).attr("src",
@@ -23,7 +29,14 @@ $(document).ready(function() {
                 );
             }
             let score = data[side]["score"];
-            $("#score-" + side).html(score);
+            update($("#score-" + side), score);
+            for (let i = 0; i < 3; i++) {
+                if (score > i) {
+                    $(".points." + side + " > .point")[i].addClass("on");
+                } else {
+                    $(".points." + side + " > .point")[i].removeClass("on");
+                }
+            }
         }
     }, "json");
 });
